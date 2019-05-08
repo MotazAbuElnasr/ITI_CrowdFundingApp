c8876c94daf1c18385687c086e141e211c7f2e0f
MotazAbuElnasr <motazibrahim@outlook.com>
2019-05-08 15:31:47 +0200

    fix errors

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.template.defaultfilters import slugify
 
-# Create your models here.
+class Project(models.Model):
+    title = models.CharField(max_length=100)
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    details = models.TextField()
+    body = models.CharField(max_length=400)
+
+class Images(models.Model):
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    def __str__(self):
+       return project.title
