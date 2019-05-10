d446534e5bf2e366ad3c1b87db20862c39f629b5
ahmedmagdyy <ahmedmagdy2016@gmail.com>
2019-05-10 19:33:19 +0200

    Activation email configurations

--- users/views.py
+++ users/views.py
@@ -18,7 +18,7 @@ def special(request):
 @login_required
 def user_logout(request):
     logout(request)
-    return HttpResponseRedirect(reverse('index'))
+    return HttpResponseRedirect('/users/user_login')
 
 
 def register(request):
