d1c0080567925b2bd4b7202669ce5920dfcd5862
ahmedmagdyy <ahmedmagdy2016@gmail.com>
2019-05-13 15:21:42 +0200

    add some styling, validating phone and email

--- users/models.py
+++ users/models.py
@@ -1,3 +1,4 @@
+from django.core.validators import RegexValidator
 from django.db import models
 from django.contrib.auth.models import User
 
@@ -6,7 +7,10 @@ from django.contrib.auth.models import User
 
 class UserProfileInfo(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    phone = models.CharField(max_length=11)
+    phone = models.CharField(
+        max_length=11,
+        validators=[RegexValidator('(01)[0-9]{9}',
+                                   message="phone number is not valid")])
     profile_pic = models.ImageField(upload_to='profile_pics', blank=True)
 
 
